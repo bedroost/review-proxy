08a25b723e0191583c409e509b9e94498c852ea1
ctong5 <cassandra.tong5@gmail.com>
2019-07-15 20:39:38 -0700

    Load all bundles on proxy port 3000

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use('/rooms/:listingid', express.static(path.join(__dirname, '../client/public')));
+app.use('/rooms/:listingid', express.static(path.join(__dirname)));
 
 app.listen(port, () => {
   console.log(`review-proxy listening on port ${port}`);
