161ba6062c461b9de31f78733fbaae55d11fe90b
ctong5 <cassandra.tong5@gmail.com>
2019-07-15 19:18:00 -0700

    Serve proxy index.html

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const port = 3000;
+const path = require('path');
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use('/rooms/:listingid', express.static(path.join(__dirname, '../client/public')));
+
+app.listen(port, () => {
+  console.log(`review-proxy listening on port ${port}`);
+});
