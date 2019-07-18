6877cb7e0e5c70cd2ece29e80eff1c5f29adaa16
ctong5 <cassandra.tong5@gmail.com>
2019-07-18 16:48:17 -0700

    Handle API requests from booking, gallery, and review EC2 deployed services.

--- index.js
+++ index.js
@@ -4,14 +4,34 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const port = 3000;
 const path = require('path');
+const axios = require('axios');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/rooms/:listingid', express.static(path.join(__dirname)));
 
-// app.get('/bundle', (res, req) => {
-//   res.sendFile(path.resolve('./modules/gallery/public/dist/bundle.js'));
-// });
+// Booking
+app.get('/api/:listingid/booking', (req, res) => {
+  axios.get(`http://ec2-13-52-61-34.us-west-1.compute.amazonaws.com/api/${req.params.listingid}/booking`)
+    .then(result => res.send(result.data))
+    .catch(error => console.log(error));
+});
+
+// Descriptions
+
+// Gallery 
+app.get('/api/:listingid/images', (req, res) => {
+  axios.get(`http://ec2-54-215-150-88.us-west-1.compute.amazonaws.com/api/${req.params.listingid}/images`)
+    .then(result => res.send(result.data))
+    .catch(error => console.log(error));
+});
+
+// Reviews
+app.get('/api/:listingid/reviews', (req, res) => {
+  axios.get(`http://ec2-13-57-195-146.us-west-1.compute.amazonaws.com/api/${req.params.listingid}/reviews`)
+    .then(result => res.send(result.data))
+    .catch(error => console.log(error));
+});
 
 app.listen(port, () => {
   console.log(`review-proxy listening on port ${port}`);
