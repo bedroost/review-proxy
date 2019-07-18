e14eac4c50ff4f6d88ee240b70ad69cce8dac4d6
ctong5 <cassandra.tong5@gmail.com>
2019-07-18 11:11:50 -0700

    Allow proxy server to locally host all 3 services on 3000 for deliverable

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/rooms/:listingid', express.static(path.join(__dirname)));
 
+// app.get('/bundle', (res, req) => {
+//   res.sendFile(path.resolve('./modules/gallery/public/dist/bundle.js'));
+// });
+
 app.listen(port, () => {
   console.log(`review-proxy listening on port ${port}`);
 });
